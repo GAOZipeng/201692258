0069591e5f07280f18d128ce68413bdcaf8f88d2
GAO Zipeng <gao_zipeng@foxmail.com>
2018-12-13 18:39:02 +0800

    edian.c注释
    edian.c 注释

---
+++ endian.c
@@ -0,0 +1,43 @@
+/* Copyright (C) Uppsala University
+ *
+ * This file is distributed under the terms of the GNU general Public
+ * License (GPL), see the file LICENSE
+ *
+ * Author: Bj�rn Wiberg <bjorn.wiberg@home.se>
+ */
+
+/*
+  This code was partially borrowed from Per Kristian Hove
+  <Per.Hove@math.ntnu.no>, who originally posted it to the rdesktop
+  mailing list at rdesktop.org.
+
+  It solves the problem of non-existent <endian.h> on some systems by
+  generating it.
+
+  (Compile, run, and redirect the output to endian.h.)
+*/
+
+//判断大小端
+
+#include <stdio.h>
+
+int litend(void)  { //小端判断
+	int i = 0;
+	((char *)(&i))[0] = 1;
+	return (i == 1);
+}
+
+int bigend(void) //大端判断
+{
+	return !litend();
+}
+
+int main(int argc, char **argv)
+{
+	printf("#ifndef _DSR_ENDIAN_H\n");
+	printf("#define _DSR_ENDIAN_H\n");
+	printf("#define __%s_ENDIAN_BITFIELD 1234\n",
+	       litend()? "LITTLE" : "BIG"); //输出大小端宏
+	printf("#endif\n");
+	return 0;
+}
